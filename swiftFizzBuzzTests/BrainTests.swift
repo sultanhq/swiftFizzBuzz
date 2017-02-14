60c910d6d7a40f3f49f73974adf09bb55e94e886
Rob Adams <robert.andrew.adams@gmail.com>
2017-02-14 14:04:39 +0000

    Swift Init

---
+++ swiftFizzBuzzTests/BrainTests.swift
@@ -0,0 +1,24 @@
+//
+//  BrainTests.swift
+//  swiftFizzBuzz
+//
+//  Created by Rob Adams on 14/02/2017.
+//  Copyright © 2017 Rob Adams. All rights reserved.
+//
+
+import XCTest
+
+class BrainTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+        
+}
