18f89acaf98ec7e5ea14db42e5ac7d2b8576b4e6
Rob Adams <robert.andrew.adams@gmail.com>
2017-02-14 20:47:23 +0000

    added game score variable

---
+++ swiftFizzBuzz/Game.swift
@@ -0,0 +1,18 @@
+//
+//  Game.swift
+//  swiftFizzBuzz
+//
+//  Created by Rob Adams on 14/02/2017.
+//  Copyright © 2017 Rob Adams. All rights reserved.
+//
+
+import Foundation
+
+class Game {
+    
+    var score: Int
+    
+    init() {
+        score = 0
+    }
+}
---
+++ swiftFizzBuzzTests/GameTests.swift
@@ -0,0 +1,29 @@
+//
+//  GameTests.swift
+//  swiftFizzBuzz
+//
+//  Created by Rob Adams on 14/02/2017.
+//  Copyright © 2017 Rob Adams. All rights reserved.
+//
+
+import XCTest
+@testable import swiftFizzBuzz
+
+class GameTests: XCTestCase {
+    
+    let game = Game()
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testsGameStartsAtZero() {
+        XCTAssertTrue(game.score == 0)
+        
+    }
+    
+}
