89b98f5fd85c6fcaaa17f7ba879730666faa2c36
Rob Adams <robert.andrew.adams@gmail.com>
2017-02-16 00:39:17 +0000

    added fizz buzz tests and wrong answer test

--- swiftFizzBuzzTests/ViewControllerUnitTests.swift
+++ swiftFizzBuzzTests/ViewControllerUnitTests.swift
@@ -45,4 +45,32 @@ class ViewControllerUnitTests: XCTestCase {
         
     }
     
+    func testFizzIncreamentScore() {
+        viewController.game?.score = 2
+        viewController.play(move: "Fizz")
+        let newScore = viewController.gameScore
+        XCTAssertEqual(newScore, 3)
+    }
+    
+    func testBuzzIncreamentScore() {
+        viewController.game?.score = 4
+        viewController.play(move: "Buzz")
+        let newScore = viewController.gameScore
+        XCTAssertEqual(newScore, 5)
+    }
+    
+    func testFizzBuzzIncreamentScore() {
+        viewController.game?.score = 14
+        viewController.play(move: "FizzBuzz")
+        let newScore = viewController.gameScore
+        XCTAssertEqual(newScore, 15)
+    }
+    
+    func testFizzOn1NotIncreamentScore() {
+        viewController.game?.score = 1
+        viewController.play(move: "Fizz")
+        let newScore = viewController.gameScore
+        XCTAssertEqual(newScore, 1)
+    }
+    
 }
